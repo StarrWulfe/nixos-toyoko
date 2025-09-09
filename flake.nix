70a61a63756134362e0b6025de1886e96a0350b5
StarrWulfe (JLGatewood) <jlgatewood@gmail.com>
2025-09-09 01:23:46 -0400

    flake: add homeConfigurations.toyoko respectfully.

--- flake.nix
+++ flake.nix
@@ -13,7 +13,20 @@
   outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+
+      # Overlay to replace youtube-dl with yt-dlp everywhere
+      overlay-ytfix = (final: prev: {
+        youtube-dl = final.yt-dlp;
+        python3Packages = prev.python3Packages // {
+          youtube-dl = final.python3Packages.yt-dlp;
+        };
+      });
+
+      # Shared package set with overlay applied
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ overlay-ytfix ];
+      };
     in {
       # Home Manager standalone config
       homeConfigurations.toyoko = home-manager.lib.homeManagerConfiguration {
@@ -30,33 +43,10 @@
           ./configuration.nix
           home-manager.nixosModules.home-manager
           {
+            nixpkgs.overlays = [ overlay-ytfix ];
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.j7 = import ./home.nix;
-          }
-        ];
-      };
-    };
-      overlays = [
-        (final: prev: {
-          python3Packages = prev.python3Packages // {
-            youtube-dl = final.yt-dlp;
-          };
-        })
-      ];
-    in {
-      nixosConfigurations.toyoko = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.j7 = import ./modules/home/default.nix;
-              backupFileExtension = "backup";
-            };
           }
         ];
       };
