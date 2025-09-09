861b8a8410e2526f5b5daad1329bcd3d19415930
StarrWulfe (JLGatewood) <jlgatewood@gmail.com>
2025-09-09 02:00:20 -0400

    creating test runs branch for troubleshooting

---
+++ test-runs/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Minimal test flake for Home Manager";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      homeConfigurations.toyoko = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./home.nix
+        ];
+      };
+    };
+}
